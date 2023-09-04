a8403440f39bbe60730f9d995c1d3916b551a0b0
dophelps <dophelps@gmail.com>
2023-09-04 15:51:27 -0700

    Added opened-source information from Github

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
 import "./App.css";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <h1> Weather App</h1>
+      <footer>
+        This project was coded by <a href="">Dani Phelps</a> and it is
+        <a
+          href="https://github.com/dophelps/react-weather-app"
+          target="blank"
+        ></a>
+        opened-source on GitHub.
+      </footer>
+    </div>
+  );
 }
 
 export default App;
